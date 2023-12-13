8708e6cfcb2dacd91d1b14562db10b45ec1a63a0
Jooseeruu <arjona2908@gmail.com>
2023-12-13 16:31:36 +0100

    update

--- db/delete.py
+++
@@ -1,9 +0,0 @@
-from conexion import conectar
-
-def eliminar_por_fecha(fecha):
-    try:
-        coleccion = conectar()
-        coleccion.delete_many({"Fecha": fecha})
-        print("Datos eliminados correctamente.")
-    except Exception as e:
-        print("Error al eliminar datos:", e)
--- db/update.py
+++
@@ -1,9 +0,0 @@
-from conexion import conectar
-
-def actualizar_asistencia(fecha, nueva_asistencia):
-    try:
-        coleccion = conectar()
-        coleccion.update_many({"Fecha": fecha}, {"$set": nueva_asistencia})
-        print("Datos actualizados correctamente.")
-    except Exception as e:
-        print("Error al actualizar datos:", e)
