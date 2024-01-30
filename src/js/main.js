261b3f6b2d39d7b83beb2a7cefcd6001a742f247
jooseeruu <arjona2908@gmail.com>
2024-01-30 14:40:18 +0100

    más simple

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,43 @@
-import { cargarDatosAsistencia } from './asistencia.js';
+document.addEventListener("DOMContentLoaded", async () => {
+  const botones = {
+    porcentajeButton: "Porcentaje_faltas",
+    faltasButton: "Faltas_no_justificadas",
+    faltasRestantesButton: "Faltas_restantes",
+    valorPorcentajeFaltaButton: "Valor_porcentaje_falta"
+  };
 
-// Asigna los manejadores de eventos después de que el DOM esté completamente cargado
-window.addEventListener('DOMContentLoaded', (event) => {
-    // Ordena por porcentaje por defecto
-    cargarDatosAsistencia('porcentaje');
+  await cargarDatosAsistencia("Porcentaje_faltas");
+  Object.keys(botones).forEach((id) => {
+    document
+      .querySelector(`#${id}`)
+      .addEventListener("click", () => cargarDatosAsistencia(botones[id]));
+  });
 
-    // Asigna los manejadores de eventos a los botones
-    document.querySelector('#porcentajeButton').addEventListener('click', () => cargarDatosAsistencia('porcentaje'));
-    document.querySelector('#faltasButton').addEventListener('click', () => cargarDatosAsistencia('faltas'));
-    document.querySelector('#faltasRestantesButton').addEventListener('click', () => cargarDatosAsistencia('faltas_restantes'));
-    document.querySelector('#valorPorcentajeFaltaButton').addEventListener('click', () => cargarDatosAsistencia('valor_porcentaje_falta'));
+  async function cargarDatosAsistencia(orden) {
+    const respuesta = await fetch("data/asistencia.json");
+    const datos = await respuesta.json();
+    const datosOrdenados = ordenarDatos(datos, orden);
+    crearTabla(datosOrdenados, datos);
+  }
+
+  function ordenarDatos(datos, orden) {
+    return Object.entries(datos[orden]).sort((a, b) => b[1] - a[1]);
+  }
+
+  function crearTabla(datosOrdenados, datos) {
+    const tabla = document.getElementById("asistenciaTable");
+    // Limpiar las filas existentes
+    tabla.innerHTML =
+      "<tr><th>Asignatura</th><th>Faltas</th><th>Porcentaje de Faltas</th><th>Faltas Restantes</th><th>Impacto por Falta</th></tr>";
+    datosOrdenados.forEach(([asignatura]) => {
+      const fila = tabla.insertRow(-1);
+      fila.innerHTML = `
+                <td>${asignatura}</td>
+                <td>${datos["Faltas_no_justificadas"][asignatura]}</td>
+                <td>${(datos["Porcentaje_faltas"][asignatura] * 1).toFixed(2)}%</td>
+                <td>${datos["Faltas_restantes"][asignatura]}</td>
+                <td>${(datos["Valor_porcentaje_falta"][asignatura] * 1).toFixed(2)}%</td>
+            `;
+    });
+  }
 });
-
